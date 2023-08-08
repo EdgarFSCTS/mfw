1bac726a7c7a6bd98c67ac5e7bdb8905f3cdc55c
Edgar Patricio <Edgar@foodsafetycts.com>
2023-08-08 15:54:04 -0600

    Session Module created

---
+++ src/Magma/Session/Exception/SessionStorageInvalidArgumentException.php
@@ -0,0 +1,13 @@
+<?php 
+
+declare(strict_types=1); 
+
+namespace Magma\Session\Exception;
+
+use InvalidArgumentException;
+
+class SessionStorageInvalidArgumentException extends InvalidArgumentException
+{
+} 
+
+?>
---
+++ src/Magma/Session/Storage/SessionStorageInterface.php
@@ -0,0 +1,36 @@
+<?php 
+
+declare(strict_types=1); 
+
+namespace Magma\Session\Storage;
+
+interface SessionStorageInterface
+{
+
+  public function setSessionName(string $sessionName) : void;
+
+  public function getSessionName(): string;
+
+  public function setSessionID(string $sessionID) : void;
+
+  public function getSessionID();
+
+  public function setSession(string $key, $value) : void;
+
+  public function setArraySession(string $key, $value) : void;
+
+  public function getSession(string $key, $default = null);
+
+  public function deleteSession(string $key) : void;
+
+  public function invalidate(): void;
+
+  public function flush(string $key, $default = null);
+
+  public function hasSession(string $key) : bool;
+
+  
+
+}
+
+?>
